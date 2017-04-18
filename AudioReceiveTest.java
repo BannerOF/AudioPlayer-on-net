7a3d7c7ccb22748b96e3e09ca01045989dac32c5
BannerOF <wangzq96@126.com>
2017-04-18 22:15:40 +0800

    提交AudioReceiveTest

---
+++ AudioReceiveTest.java
@@ -0,0 +1,20 @@
+import java.io.File;
+import java.net.*;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.SourceDataLine;
+
+public class AudioReceiveTest implements Runnable
+{
+	public void run()
+	{
+
+	}
+	public AudioReceiveTest(int tPort, int Port, String tAddress)
+	{
+
+	}
+}
