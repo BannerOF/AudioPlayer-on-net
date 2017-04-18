e113a3af76e85bee9341af1e2a63ec42b62da369
BannerOF <wangzq96@126.com>
2017-04-18 20:50:12 +0800

    提交AudioSendTest更新

--- AudioSendTest.java
+++ AudioSendTest.java
@@ -7,18 +7,45 @@ import javax.sound.sampled.DataLine;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.SourceDataLine;
 
-public class AudioSendTest
+public class AudioSendTest implements Runnable
 {
 	private ASoNProtocol NetSend; 
+	private AudioInputStream audioInputStream;
+	private AudioFormat audioFormat;
 
 	public AudioSendTest(int tPort, int Port, String tAddress)
 	{
 		try{
 			NetSend = new ASoNProtocol(tPort, Port, InetAddress.getByName(tAddress));
+			File file = new File("/test.wav");
+			audioInputStream = AudioSystem.getAudioInputStream(file);
+			audioFormat = audioInputStream.getFormat(); 
+			if(audioFormat.getEncoding() != AudioFormat.Encoding.PCM_SIGNED)
+			{
+				audioFormat = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED,audioFormat.getSampleRate(),
+						16,audioFormat.getChannels(),audioFormat.getChannels()*2,audioFormat.getSampleRate(),false);
+				audioInputStream = AudioSystem.getAudioInputStream(audioFormat, audioInputStream);
+			}
 		}catch(Exception e) { e.printStackTrace(); }
+	}
+	public void run()
+	{
+		byte[] readbuf = new byte[320];
+		try{
+			while(true)
+			{
+				audioInputStream.read(readbuf, 0, readbuf.length);		
+				NetSend.sendData(readbuf);	
+			}
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
 	}
 	public static void main(String args[])
 	{
-
+		AudioSendTest AST = new AudioSendTest(10010, 10010, "192.168.0.29");
+		Thread thread = new Thread(AST);
+		thread.start();	
 	}
 }
