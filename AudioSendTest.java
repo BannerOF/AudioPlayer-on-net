9f1d62e86a3bbd7549ac602531384e9d4ff8a00a
BannerOF <wangzq96@126.com>
2017-04-19 10:32:23 +0800

    提交AudioSendTest修改

--- AudioSendTest.java
+++ AudioSendTest.java
@@ -3,9 +3,7 @@ import java.net.*;
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.DataLine;
 import javax.sound.sampled.LineUnavailableException;
-import javax.sound.sampled.SourceDataLine;
 
 public class AudioSendTest implements Runnable
 {
