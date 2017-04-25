a511c0b7582527a68f7bbc50d5ddeb5211b6b226
Bannerof <wangzq96@126.com>
2017-04-25 23:45:54 +0800

    提交测试

--- AudioReceiveTest.java
+++ AudioReceiveTest.java
@@ -52,7 +52,7 @@ public class AudioReceiveTest implements Runnable
 	}//}}}
 	public static void main(String args[])//{{{
 	{
-		AudioReceiveTest ART = new AudioReceiveTest(10010, 10010, "192.168.0.2");
+		AudioReceiveTest ART = new AudioReceiveTest(10010, 10011, "127.0.0.1");
 		Thread thread = new Thread(ART);
 		thread.start();
 	}//}}}
