2ed343b1d0c45e47d9a8ae05ee43e635b90cabbc
Bannerof <wangzq96@126.com>
2017-05-07 01:03:01 +0800

    提交AudioReceiveTest AudioSendTest 文件

---
+++ AudioReceiveTest.java
@@ -0,0 +1,59 @@
+import java.io.File;
+import java.net.InetAddress;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.SourceDataLine;
+
+public class AudioReceiveTest implements Runnable
+{
+	private AudioInputStream audioInputStream;
+	private AudioFormat audioFormat;
+	private SourceDataLine sourceDataLine;
+	private DataLine.Info dataLineInfo;
+	private ASoNProtocol NetReceive;
+
+	public void run()//{{{
+	{
+		try{
+			while(true)
+			{
+				ASoNPacket packet = NetReceive.getData();
+				byte[] data = packet.getData();
+				sourceDataLine.write(data, 0, data.length);
+			}
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}//}}}
+	public AudioReceiveTest(int tPort, int Port, String tAddress)//{{{
+	{
+		try{
+			NetReceive = new ASoNProtocol(tPort, Port, InetAddress.getByName(tAddress));
+			File file = new File("/test.wav");
+			audioInputStream = AudioSystem.getAudioInputStream(file);
+			audioFormat = audioInputStream.getFormat();
+			if(audioFormat.getEncoding() != AudioFormat.Encoding.PCM_SIGNED)
+			{
+				audioFormat = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED,audioFormat.getSampleRate(),
+						16,audioFormat.getChannels(),audioFormat.getChannels()*2,audioFormat.getSampleRate(),false);
+				audioInputStream = AudioSystem.getAudioInputStream(audioFormat, audioInputStream);
+			}
+			dataLineInfo = new DataLine.Info(
+				SourceDataLine.class, audioFormat,
+				AudioSystem.NOT_SPECIFIED);
+			sourceDataLine = (SourceDataLine)AudioSystem.getLine(dataLineInfo);
+			sourceDataLine.open(audioFormat);
+			sourceDataLine.start();
+			NetReceive.startWorking();
+		}catch(Exception e){ e.printStackTrace(); }
+	}//}}}
+	public static void main(String args[])//{{{
+	{
+		AudioReceiveTest ART = new AudioReceiveTest(10010, 10011, "171.113.100.102");
+		Thread thread = new Thread(ART);
+		thread.start();
+	}//}}}
+}
---
+++ AudioSendTest.java
@@ -0,0 +1,50 @@
+import java.io.File;
+import java.net.InetAddress;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+
+public class AudioSendTest implements Runnable
+{
+	private ASoNProtocol NetSend; 
+	private AudioInputStream audioInputStream;
+	private AudioFormat audioFormat;
+
+	public void run()//{{{
+	{
+		try{
+			while(true)
+			{
+				byte[] readbuf = new byte[2000];
+				audioInputStream.read(readbuf, 0, readbuf.length);		
+				NetSend.sendData(readbuf);	
+				Thread.sleep(0,10);
+			}
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}//}}}
+	public AudioSendTest(int tPort, int Port, String tAddress)//{{{
+	{
+		try{
+			NetSend = new ASoNProtocol(tPort, Port, InetAddress.getByName(tAddress));
+			File file = new File("/test.wav");
+			audioInputStream = AudioSystem.getAudioInputStream(file);
+			audioFormat = audioInputStream.getFormat(); 
+			if(audioFormat.getEncoding() != AudioFormat.Encoding.PCM_SIGNED)
+			{
+				audioFormat = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED,audioFormat.getSampleRate(),
+						16,audioFormat.getChannels(),audioFormat.getChannels()*2,audioFormat.getSampleRate(),false);
+				audioInputStream = AudioSystem.getAudioInputStream(audioFormat, audioInputStream);
+			}
+			NetSend.startWorking();
+		}catch(Exception e) { e.printStackTrace(); }
+	}//}}}
+	public static void main(String args[])//{{{
+	{
+		AudioSendTest AST = new AudioSendTest(10011, 10010, "59.71.142.58");
+		Thread thread = new Thread(AST);
+		thread.start();	
+	}//}}}
+}
